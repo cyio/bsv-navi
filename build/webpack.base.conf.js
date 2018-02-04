a075cbd2c419864c25e0ce66a9a4def197a7456e
cyio <icaner@qq.com>
2018-02-04 16:31:08 +0800

    添加 raw loader

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -71,7 +71,11 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
-      }
+      },
+			{
+				test: /\.(txt|md)$/,
+				use: 'raw-loader'
+			}
     ]
   }
 }
