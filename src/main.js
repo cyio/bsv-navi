6a9e3a18e61585802931a0e20364d1906bddb0be
cyio <icaner@qq.com>
2018-02-07 18:02:47 +0800

    normalize css 要在 app.vue 前 import

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'normalize.css'
 import 'iview/dist/styles/iview.css'
 import App from './App'
 import router from './router'
 import VueI18n from 'vue-i18n'
 import 'vue-awesome/icons/search'
 import Icon from 'vue-awesome/components/Icon'
-import 'normalize.css'
 
 Vue.config.productionTip = false
 Vue.use(VueI18n)
