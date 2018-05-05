96f97c9ba088f4d22134e3f0058abb4d6bcce386
cyio <icaner@qq.com>
2018-05-05 17:59:58 +0800

    页面标题动态修改
    Updating Page Title & Metadata with Vue.js & vue-router ← Alligator.io https://alligator.io/vuejs/vue-router-modify-head/

--- src/router.js
+++ src/router.js
@@ -6,8 +6,45 @@ import SafeGuides from '@/pages/SafeGuides'
 
 Vue.use(Router)
 
-export default new Router({
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    meta: { keepAlive: true, title: 'BCH123_比特币现金导航' }
+  },
+  {
+    path: '/address/:id',
+    name: 'Address',
+    component: Address,
+    meta: {
+      title: '比特币现金帐户查看器_BCH123',
+      metaTags: [
+        // {
+        // name: 'description',
+        // content: 'The home page of our example app.'
+        // },
+        // {
+        // property: 'og:description',
+        // content: 'The home page of our example app.'
+        // }
+      ]
+    }
+  },
+  {
+    path: '/safe-guides',
+    name: 'SafeGuides',
+    component: SafeGuides,
+    meta: {
+      title: '数字货币安全保管使用指南_BCH123',
+    }
+  },
+  { path: '*', redirect: '/' }
+]
+
+const router = new Router({
   mode: 'hash',
+  routes,
   scrollBehavior (to, from, savedPosition) {
     // console.log('scroll', to.path, from.path, savedPosition)
     if (savedPosition) {
@@ -20,23 +57,26 @@ export default new Router({
       return { x: 0, y: 0 }
     }
   },
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home,
-      meta: { keepAlive: true }
-    },
-    {
-      path: '/address/:id',
-      name: 'Address',
-      component: Address
-    },
-    {
-      path: '/safe-guides',
-      name: 'SafeGuides',
-      component: SafeGuides
-    },
-    { path: '*', redirect: '/' }
-  ]
 })
+
+router.beforeEach((to, from, next) => {
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title)
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags)
+  // const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags)
+  if (nearestWithTitle) document.title = nearestWithTitle.meta.title
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el))
+  if (!nearestWithMeta) return next()
+  nearestWithMeta.meta.metaTags.map(tagDef => {
+    const tag = document.createElement('meta')
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key])
+    })
+    tag.setAttribute('data-vue-router-controlled', '')
+    return tag
+  })
+    .forEach(tag => document.head.appendChild(tag))
+
+  next()
+})
+
+export default router
